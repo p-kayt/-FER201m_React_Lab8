00e88fc69aab61bed40499cc43495e73196c04bc
Kayt <phamkhactrieu2002@gmail.com>
2022-06-27 16:36:03 +0700

    Single Page Applications Part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,64 +1,76 @@
-import React from 'react';
-import { Card, CardImg, CardText, CardBody,
-    CardTitle } from 'reactstrap';
+import React from "react";
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from "reactstrap";
+import { Link } from "react-router-dom";
 
-   function RenderDish({dish})  {
-      if (dish != null) {
-         return (
-            <div key={dish.id} className="col-12 col-md-5 m-1">
-               <Card>
-                  <CardImg width="100%" src={dish.image} alt={dish.name} />
-                  <CardBody>
-                     <CardTitle tag={"h5"}>{dish.name}</CardTitle>
-                     <CardText>{dish.description}</CardText>
-                  </CardBody>
-               </Card>
-            </div>
-         );
-      } else {
-         return <div></div>;
-      }
+function RenderDish({ dish }) {
+   if (dish != null) {
+      return (
+         <div key={dish.id} className="col-12 col-md-5 m-1">
+            <Card>
+               <CardImg width="100%" src={dish.image} alt={dish.name} />
+               <CardBody>
+                  <CardTitle tag={"h5"}>{dish.name}</CardTitle>
+                  <CardText>{dish.description}</CardText>
+               </CardBody>
+            </Card>
+         </div>
+      );
+   } else {
+      return <div></div>;
    }
+}
 
-   function RenderComments({comments})  {
-      if (comments != null) {
-         const showComments = comments.map((cmt) => {
-            const options = { month: "short", year: "numeric", day: "numeric" };
-            return (
-               <span key={cmt.id}>
-                  <p>{cmt.comment}</p>
-                  <p>
-                     --{cmt.author}, {new Date(cmt.date).toLocaleDateString("en-US", options)}
-                  </p>
-               </span>
-            );
-         });
-
+function RenderComments({ comments }) {
+   if (comments != null) {
+      const showComments = comments.map((cmt) => {
+         const options = { month: "short", year: "numeric", day: "numeric" };
          return (
-            <div className="col-12 col-md-5 m-1">
-               <h3>Comments</h3>
-               {showComments}
-            </div>
+            <span key={cmt.id}>
+               <p>{cmt.comment}</p>
+               <p>
+                  --{cmt.author}, {new Date(cmt.date).toLocaleDateString("en-US", options)}
+               </p>
+            </span>
          );
-      } else {
-         return <div></div>;
-      }
-   }
+      });
 
-   const  DishDetail = (props) => {
-      const dish = props.dish;
-      if (dish == null) {
-         return <div></div>;
-      } else {
-         return (
-            <div className="container">
-               <div className="row">
-                  <RenderDish dish={dish}/>
-                  <RenderComments comments={dish.comments}/>
+      return (
+         <div className="col-12 col-md-5 m-1">
+            <h3>Comments</h3>
+            {showComments}
+         </div>
+      );
+   } else {
+      return <div></div>;
+   }
+}
+
+const DishDetail = (props) => {
+   const dish = props.dish;
+   if (dish == null) {
+      return <div></div>;
+   } else {
+      return (
+         <div className="container">
+            <div className="row">
+               <Breadcrumb>
+                  <BreadcrumbItem>
+                     <Link to="/menu">Menu</Link>
+                  </BreadcrumbItem>
+                  <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+               </Breadcrumb>
+               <div className="col-12">
+                  <h3>{props.dish.name}</h3>
+                  <hr />
                </div>
             </div>
-         );
-      }
-    }
+            <div className="row">
+               <RenderDish dish={props.dish} />
+               <RenderComments comments={props.comments} />
+            </div>
+         </div>
+      );
+   }
+};
 
 export default DishDetail;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -41,6 +41,14 @@ class Main extends Component {
          );
       };
 
+      const DishWithId = ({match}) => {
+        return(
+            <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+              comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+        );
+      };
+  
+
       return (
          <div>
             {/* <Navbar dark color="primary">
@@ -53,6 +61,7 @@ class Main extends Component {
             <Switch>
                <Route path="/home" component={HomePage} />
                <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+               <Route path='/menu/:dishId' component={DishWithId} />
                <Route exact path="/contactus" component={Contact} />
                <Redirect to="/home" />
             </Switch>
